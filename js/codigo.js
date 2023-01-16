c147cfb2118391ddef2cd6dff0dbafddd342bdb1
jimmygrande <jimmygrande@gmail.com>
2023-01-16 15:03:07 -0400

    separar archivos y renombrar html

---
+++ js/codigo.js
@@ -0,0 +1,16 @@
+function aliatorio(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+let jugador = 0
+let numero = aliatorio(1, 100)
+alert("Encuentra el numero del 1 al 100")
+while (numero != jugador) {
+    jugador = prompt("Elije un numero: ")
+    if(jugador < numero) {
+        alert("Elije un numero mas alto")
+    } else if(jugador > numero) {
+        alert("Elije un numero mas bajo")
+    } else {
+        alert("Ganaste, Felicidades")
+    }
+}
