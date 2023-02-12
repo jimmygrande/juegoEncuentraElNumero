ef04f5a4a77df4956a3e25a45c68f6b216fe4c9e
jimmygrande <jimmygrande@gmail.com>
2023-02-11 23:08:29 -0400

    na

--- js/codigo.js
+++ js/codigo.js
@@ -1,8 +1,8 @@
-function aliatorio(min, max) {
+function aleatorio(min, max) {
     return Math.floor(Math.random()*(max-min+1)+min)
 }
 let jugador = 0
-let numero = aliatorio(1, 100)
+let numero = aleatorio(1, 100)
 alert("Encuentra el numero del 1 al 100")
 while (numero != jugador) {
     jugador = prompt("Elije un numero: ")
